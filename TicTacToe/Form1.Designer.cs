57755dc6a6d75502bff2d9eda2e8fe447805e197
Tarik Karaca <tarik.karaca@stud.uni-duisburg-essen.de>
2018-05-21 05:58:55 +0200

    Projektdateien hinzufügen.

---
+++ TicTacToe/Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace TicTacToe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 68);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(46, 39);
+            this.button1.TabIndex = 0;
+            this.button1.Tag = "0";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.OnSquareClick);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(64, 68);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(46, 39);
+            this.button2.TabIndex = 1;
+            this.button2.Tag = "1";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.OnSquareClick);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(116, 68);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(46, 39);
+            this.button3.TabIndex = 2;
+            this.button3.Tag = "2";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.OnSquareClick);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(12, 113);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(46, 39);
+            this.button4.TabIndex = 3;
+            this.button4.Tag = "3";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.OnSquareClick);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(64, 113);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(46, 39);
+            this.button5.TabIndex = 4;
+            this.button5.Tag = "4";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.OnSquareClick);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(116, 113);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(46, 39);
+            this.button6.TabIndex = 5;
+            this.button6.Tag = "5";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.OnSquareClick);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(12, 158);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(46, 39);
+            this.button7.TabIndex = 6;
+            this.button7.Tag = "6";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.OnSquareClick);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(64, 158);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(46, 39);
+            this.button8.TabIndex = 7;
+            this.button8.Tag = "7";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.OnSquareClick);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(116, 158);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(46, 39);
+            this.button9.TabIndex = 8;
+            this.button9.Tag = "8";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.OnSquareClick);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(12, 12);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(75, 23);
+            this.button10.TabIndex = 9;
+            this.button10.Text = "Reset";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.OnResetClick);
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(93, 12);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(69, 23);
+            this.button11.TabIndex = 10;
+            this.button11.Text = "Undo";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.OnUndoClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(177, 209);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "TicTacToe";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+    }
+}
+
---
+++ TicTacToe/Form1.cs
@@ -0,0 +1,308 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicTacToe
+{
+    public partial class Form1 : Form
+    {
+        public enum PieceType
+        {
+            X, O, None
+        }
+
+        public struct Draw
+        {
+            public PieceType piece;
+            public int squareIndex;
+        }
+
+        public struct Line {
+            public int[] indices;
+
+            public Line(int i1, int i2, int i3) {
+                indices = new int[3];
+
+                indices[0] = i1;
+                indices[1] = i2;
+                indices[2] = i3;
+            }
+        }
+
+        public class GameBoard
+        {
+            public readonly Line[] Lines = {
+                                               new Line(0, 1, 2),
+                                               new Line(3, 4, 5),
+                                               new Line(6, 7, 8),
+                                               new Line(0, 3, 6),
+                                               new Line(1, 4, 7),
+                                               new Line(2, 5, 8),
+                                               new Line(0, 4, 8),
+                                               new Line(2, 4, 6)
+                                           };
+
+            public PieceType[] squares;
+            Stack<Draw> drawHistory;
+            int winner;
+
+            public GameBoard()
+            {
+                squares = new PieceType[9];
+
+                for (int i = 0; i < 9; i++)
+                    squares[i] = PieceType.None;
+
+                drawHistory = new Stack<Draw>();
+                winner = -1;
+            }
+
+            public bool GameEnded()
+            {
+                foreach (Line line in Lines)
+                {
+                    int nPlayer = 0;
+                    int nAI = 0;
+                    for (int i = 0; i < 3; i++)
+                    {
+                        if (squares[line.indices[i]] == PieceType.X)
+                            nPlayer++;
+                        else if (squares[line.indices[i]] == PieceType.O)
+                            nAI++;
+                    }
+
+                    if (nPlayer == 3)
+                        winner = 0;
+                    if (nAI == 3)
+                        winner = 1;
+
+                    if (nPlayer == 3 || nAI == 3)
+                        return true;
+                }
+
+                int nEmpty = 0;
+                for (int i = 0; i < 9; i++)
+                {
+                    if (squares[i] == PieceType.None)
+                        nEmpty++;
+                }
+
+                return (nEmpty == 0);
+            }
+
+            public int GetWinner()
+            {
+                return winner;
+            }
+
+            public void MakeDraw(PieceType piece, int index)
+            {
+                if (piece == PieceType.None)
+                    return;
+
+                if (squares[index] != PieceType.None)
+                    return;
+
+                Draw draw;
+                draw.piece = piece;
+                draw.squareIndex = index;
+
+                drawHistory.Push(draw);
+                squares[index] = piece;
+            }
+
+            public void UndoDraw()
+            {
+                if (drawHistory.Count == 0)
+                    return;
+                Draw lastDraw = drawHistory.Pop();
+                squares[lastDraw.squareIndex] = PieceType.None;
+            }
+
+            public void GetLegalDraws(int player, ref List<Draw> draws)
+            {
+                draws.Clear();
+
+                if (GameEnded())
+                    return;
+
+                for (int i = 0; i < 9; i++)
+                {
+                    if (squares[i] == PieceType.None)
+                    {
+                        Draw draw;
+                        draw.piece = (player == 0 ? PieceType.X : PieceType.O);
+                        draw.squareIndex = i;
+                        draws.Add(draw);
+                    }
+                }
+            }
+        }
+
+        private readonly String[] PieceNames = { "X", "O", " " };
+
+        private Button[] squareButtons;
+        private GameBoard gameBoard;
+        private int maxDepth;
+        private Draw currentAIDraw;
+
+        public Form1()
+        {
+            InitializeComponent();
+            Init();
+        }
+
+        public void Init()
+        {
+            squareButtons = new Button[9];
+            squareButtons[0] = button1;
+            squareButtons[1] = button2;
+            squareButtons[2] = button3;
+            squareButtons[3] = button4;
+            squareButtons[4] = button5;
+            squareButtons[5] = button6;
+            squareButtons[6] = button7;
+            squareButtons[7] = button8;
+            squareButtons[8] = button9;
+
+            gameBoard = new GameBoard();
+            UpdateBoard();
+        }
+
+        private int GetIndex(int column, int row)
+        {
+            return column + row * 3;
+        }
+
+        private void UpdateBoard()
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                PieceType piece = gameBoard.squares[i];
+                squareButtons[i].Text = PieceNames[(int)piece];
+                squareButtons[i].Enabled = (piece == PieceType.None);
+            }
+        }
+
+        private void AIDraw()
+        {
+            maxDepth = 2;
+            Max(1, maxDepth);
+            gameBoard.MakeDraw(currentAIDraw.piece, currentAIDraw.squareIndex);
+        }
+
+        private int Min(int player, int depth)
+        {
+            List<Draw> legalDraws = new List<Draw>();
+            gameBoard.GetLegalDraws(player, ref legalDraws);
+            if (depth == 0 || legalDraws.Count == 0)
+                return Rate();
+
+            int minValue = int.MaxValue;
+            foreach (Draw draw in legalDraws)
+            {
+                gameBoard.MakeDraw(draw.piece, draw.squareIndex);
+                int value = Min((player + 1) % 2, depth - 1);
+                gameBoard.UndoDraw();
+                if (value < minValue)
+                {
+                    minValue = value;
+                    if (depth == maxDepth)
+                        currentAIDraw = draw;
+                }
+            }
+            return minValue;
+        }
+
+        private int Max(int player, int depth)
+        {
+            List<Draw> legalDraws = new List<Draw>();
+            gameBoard.GetLegalDraws(player, ref legalDraws);
+            if (depth == 0 || legalDraws.Count == 0)
+                return Rate();
+
+            int maxValue = int.MinValue;
+            foreach (Draw draw in legalDraws)
+            {
+                gameBoard.MakeDraw(draw.piece, draw.squareIndex);
+                int value = Min((player + 1) % 2, depth - 1);
+                gameBoard.UndoDraw();
+                if (value > maxValue)
+                {
+                    maxValue = value;
+                    if (depth == maxDepth)
+                        currentAIDraw = draw;
+                }
+            }
+            return maxValue;
+        }
+
+        private int Rate()
+        {
+            int rating = 0;
+            foreach (Line line in gameBoard.Lines) {
+                int nPlayer = 0;
+                int nAI = 0;
+                for (int i = 0; i < 3; i++)
+                {
+                    if (gameBoard.squares[line.indices[i]] == PieceType.X)
+                        nPlayer++;
+                    else if (gameBoard.squares[line.indices[i]] == PieceType.O)
+                        nAI++;
+                }
+
+                if (nAI == 0)
+                {
+                    if (nPlayer == 0)
+                        rating++;
+                    else if (nPlayer == 1)
+                        rating += -10;
+                    else if (nPlayer == 2)
+                        rating += -100;
+                    else if (nPlayer == 3)
+                        rating += -1000;
+                }
+                else if (nPlayer == 0)
+                {
+                    if (nAI == 1)
+                        rating += 10;
+                    else if (nAI == 2)
+                        rating += 100;
+                    else if (nAI == 3)
+                        rating += 1000;
+                }
+            }
+            return rating;
+        }
+
+        private void OnSquareClick(object sender, EventArgs e)
+        {
+            if (gameBoard.GameEnded())
+                return;
+
+            Button square = (Button)sender;
+            int index = int.Parse(square.Tag.ToString());
+            gameBoard.MakeDraw(PieceType.X, index);
+            AIDraw();
+            UpdateBoard();
+        }
+
+        private void OnResetClick(object sender, EventArgs e)
+        {
+            Init();
+        }
+
+        private void OnUndoClick(object sender, EventArgs e)
+        {
+            gameBoard.UndoDraw();
+            gameBoard.UndoDraw();
+            UpdateBoard();
+        }
+    }
+}
